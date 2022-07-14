00417773d371161c965069b4da726d4b515bf55a
Duc <ngoduc2277@gmail.com>
2022-07-14 09:25:33 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      spacing: {
+        big: "48rem",
+      },
+    },
+
+    fontFamilys: {
+      nunito: ["Nunito", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
